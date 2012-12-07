1d4b4fa1709a8ce904bc5e8a87decf008740eb19
Thiago M Rocha <kimobr@gmail.com>
2012-12-07 04:23:30 -0800

    Create code.java
    Add questions 11539139 code

---
+++ android/camera/11539139/code.java
@@ -0,0 +1,49 @@
+public class MainActivity extends Activity implements SurfaceTextureListener{
+
+    private Camera mCamera;
+    private TextureView mTextureView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mTextureView = new TextureView(this);
+        mTextureView.setSurfaceTextureListener(this);
+
+        setContentView(mTextureView);
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        mCamera = Camera.open();
+
+        Camera.Size previewSize = mCamera.getParameters().getPreviewSize();
+        mTextureView.setLayoutParams(new FrameLayout.LayoutParams(
+                previewSize.width, previewSize.height, Gravity.CENTER));
+
+        try {
+            mCamera.setPreviewTexture(surface);
+        } catch (IOException t) {
+        }
+
+        mCamera.startPreview();
+
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        // Ignored, the Camera does all the work for us
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        mCamera.stopPreview();
+        mCamera.release();
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        // Update your view here!
+    }
+}
